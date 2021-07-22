d2b0dd0f1ef60623038fd1bf2f6ef64c96f420ee
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-22 15:47:17 -0500

    playing with front page

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Todeux.MixProject do
   def application do
     [
       mod: {Todeux.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :oauth2]
     ]
   end
 
@@ -46,7 +46,8 @@ defmodule Todeux.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:oauth2, "~> 2.0"}
     ]
   end
 
